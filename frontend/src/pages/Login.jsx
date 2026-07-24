8bcf9130d8eed08fa174a085299599072a05035f
A-Teja-02 <adusumilliteja02@gmail.com>
2026-07-24 12:03:56 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ import logoImg from '../assets/logo.png';
 // import slide1 from '../assets/slide1.jpg';
 // import slide2 from '../assets/slide2.jpg';
 // import slide3 from '../assets/slide3.jpg';
-// import bg from '../assets/bg.png';
+// import bg from '../assets/BG.png';
 import loginBg from '../assets/login-bg.jpg';
 
 const Login = () => {
